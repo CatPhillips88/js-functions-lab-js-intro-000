f89b9de7209af96a571d1ea84f6ffb6e4c785ded
Catherine Phillips <catherineep4189@gmail.com>
2019-06-23 16:18:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountfown(holiday, days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
